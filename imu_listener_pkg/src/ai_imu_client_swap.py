Fixed uncertainty y-axis negative sign bug

imu_listener_pkg/src/ai_imu_client_swap.py
@@ -131,8 +131,8 @@ class AIClientROS:
         ai_gyro_noise_model = np.asarray(gyro_var_all[-1], dtype=np.float64) #same for gyro
 
         # Map model output [z, -y, x] back to MAVROS [x, y, z]
-        ai_acc_noise = ai_acc_noise_model[[2, 1, 0]] * np.array([1.0, -1.0, 1.0], dtype=np.float64)
-        ai_gyro_noise = ai_gyro_noise_model[[2, 1, 0]] * np.array([1.0, -1.0, 1.0], dtype=np.float64)
+        ai_acc_noise = ai_acc_noise_model[[2, 1, 0]] * np.array([1.0, 1.0, 1.0], dtype=np.float64)
+        ai_gyro_noise = ai_gyro_noise_model[[2, 1, 0]] * np.array([1.0, 1.0, 1.0], dtype=np.float64)
 
         if not (np.isfinite(ai_acc_noise).all() and np.isfinite(ai_gyro_noise).all()):
             rospy.logerr("[AI Client] NaN/Inf output, dropping")